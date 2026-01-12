be44bc043c087598125132aab55c46239feabc3c
deramazesaa-web <deramazesaa@gmail.com>
2026-01-12 03:48:58 +0300

    Add deontic_engine module to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+pub mod deontic_engine;
 /* * CRYSTALLINE PROTOCOL: Proof of Concept (v0.1.0)
  * -----------------------------------------------
  * This PoC demonstrates the "Logic Firewall" mechanism.
