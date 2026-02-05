54815cd38721cfde6e3e35c7efeb7f1ed418b453
Elbatzy <deramazesaa@gmail.com>
2026-02-05 14:46:33 +0300

    Delete src/protocol.rs

--- src/protocol.rs
+++
@@ -1,6 +0,0 @@
-/// Universal trait for Crystalline security operations
-pub trait CrystallineSecure {
-    fn sign_data(&self, data: &[u8]) -> Vec<u8>;
-    fn verify_data(&self, data: &[u8], signature: &[u8]) -> bool;
-    fn encrypt_for_ecosystem(&self, data: &[u8], recipient_key: &[u8]) -> Vec<u8>;
-}
